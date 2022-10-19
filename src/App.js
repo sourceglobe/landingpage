08860a8bea6fcc4eefd2b2e4eea8287bc924fdc6
Rob van der Leek <robvanderleek@gmail.com>
2022-10-19 18:07:45 +0200

    Add user and country count

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {AppBar, styled, Toolbar} from "@mui/material";
 import heroImage from './static/hero-image.jpg';
 import logo from './static/sourceglobe-logo-white-on-transparent-128x128.png';
+import {useEffect, useState} from "react";
 
 const HeroSection = styled('div')`
   height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   background-image: url(${heroImage});
@@ -15,11 +17,38 @@ const HeroSection = styled('div')`
   text-align: center;
 `;
 
+const ScoreboardSection = styled('div')`
+  display: flex;
+`
+
+const ScorboardEntry = styled('div')`
+  margin: 1em;
+  display: flex;
+  flex-direction: column;
+`
+
 const Page = styled('div')`
   background-color: rgb(14, 17, 22);
 `;
 
 function App() {
+    const [userCount, setUserCount] = useState(0);
+
+    useEffect(() => {
+        const loodScoreboard = async () => {
+            const res = await fetch('https://api.github.com/gists/b744ce6d70adc12aa958080702868826')
+            if (res.ok) {
+                const json = await res.json();
+                const content = json.files['totals.json'].content;
+                const data = JSON.parse(content);
+                setUserCount(data.Users);
+            }
+        }
+        loodScoreboard();
+    }, []);
+
+
+
     return (
         <Page>
             <AppBar position="fixed" style={{background: 'none'}} elevation={0}>
@@ -29,6 +58,16 @@ function App() {
             </AppBar>
             <HeroSection>
                 Join the Sourceglobe<br/>code repository
+                <ScoreboardSection>
+                    <ScorboardEntry>
+                        <div>#Users</div>
+                        <div>{userCount}</div>
+                    </ScorboardEntry>
+                    <ScorboardEntry>
+                        <div>#Countries</div>
+                        <div>0</div>
+                    </ScorboardEntry>
+                </ScoreboardSection>
             </HeroSection>
             <div>
                 Photo by <a
